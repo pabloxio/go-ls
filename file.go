b90b36736a4b792a12cf698c028772fe18e83160
Pablo Jaramillo <juan@pablox.io>
2023-05-20 21:07:04 -0600

    Support for user/group files ownership

--- file.go
+++ file.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io/fs"
+	"os"
+	"os/user"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -22,15 +26,38 @@ func newFile(entry fs.DirEntry) (*file, error) {
 		return nil, err
 	}
 
+	user, group := getUserAndGroupOwners(&info)
+
 	return &file{
 		name:             entry.Name(),
 		isHidden:         strings.HasPrefix(entry.Name(), "."),
 		mode:             info.Mode().String(),
+		user:             user,
+		group:            group,
 		size:             info.Size(),
 		modificationTime: info.ModTime(),
-
-		// FIXME: hardcoded user and group
-		user:  "user",
-		group: "group",
 	}, nil
+}
+
+func getUserAndGroupOwners(fileInfo *os.FileInfo) (userName, groupName string) {
+	statSyscall := (*fileInfo).Sys().(*syscall.Stat_t)
+
+	userName = fmt.Sprint(statSyscall.Uid)
+	groupName = fmt.Sprint(statSyscall.Gid)
+
+	// Getting the user
+	owner, err := user.LookupId(userName)
+	if err != nil {
+		return
+	}
+	userName = owner.Username
+
+	// Getting the group
+	group, err := user.LookupGroupId(groupName)
+	if err != nil {
+		return
+	}
+	groupName = group.Name
+
+	return
 }
